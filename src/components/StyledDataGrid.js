acf8c20833ffda00115f67c81e2b88a7af928304
Mohammed Boudad <mboudad7@gmail.com>
2022-03-15 11:02:47 +0100

    style fixes

--- src/components/StyledDataGrid.js
+++ src/components/StyledDataGrid.js
@@ -8,6 +8,12 @@ export const StyledDataGrid = styled(DataGridPro)(({ theme }) => ({
   },
   '& [data-field="id"]': {
     backgroundColor: "#afd4ff",
+    "& .MuiTypography-root": {
+      fontWeight: "500",
+    },
+    borderBottom: `1px solid ${
+      theme.palette.mode === "light" ? "#e5e5e5" : "#303030"
+    }`,
   },
   "& .MuiDataGrid-columnHeader, .MuiDataGrid-cell": {
     borderRight: `1px solid ${
